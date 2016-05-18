11b99f7cf7673783abd8d211061ac1aad48cf5eb
keirstenmachine <keirstenvanhoek@gmail.com>
2016-05-17 18:41:08 -0600

    View Controller Set Up

---
+++ ShoppingList/GroceryTaskViewController.swift
@@ -0,0 +1,34 @@
+//
+//  GroceryTaskViewController.swift
+//  ShoppingList
+//
+//  Created by Keirsten Van Hoek on 5/17/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class GroceryTaskViewController: UIViewController {
+
+
+    @IBAction func addTaskName(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func addTask(sender: AnyObject) {
+        
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
